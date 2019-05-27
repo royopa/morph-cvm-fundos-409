5b9695fea5c5220af365dc6ce111e5b66fad4f11
Rodrigo Prado <royopa@gmail.com>
2019-05-26 21:59:54 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -107,7 +107,7 @@ def process_import(file_name_server, file_name_extracted):
 
 def main():
     # format the name of database used for morph.io
-    os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
+    # os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
     
     # create download folder
     create_download_folder()
